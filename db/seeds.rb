c33a62afdac9c6226b33e607e7c84f486e856b92
Maria Herrera <mariluz.herrera@gmail.com>
2019-11-14 15:23:42 +0000

    created cocktails and doses controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,9 @@
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "sugar")
+Ingredient.create(name: "orange peel")
+Ingredient.create(name: "pomegranate")
+Ingredient.create(name: "berries")
+
+p Ingredient.all
